1c3e09336720cc381d248cba61d6eb64f12bcfd7
iuval <iuval.goldansky@neonroots.com>
2013-10-20 16:25:12 -0200

    It all begins.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BStorm::Application.config.secret_key_base = '0d7989f35368b90b6cebbc8f5a21ce1e51c695f2fb99b3f3925cf0d23c9d20f3a96cb80adc11914502331a006d7e6a3adeca2e1f7d39de0b7ae115eaf91c6e72'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BStorm::Application.config.session_store :cookie_store, key: '_BStorm_session'
